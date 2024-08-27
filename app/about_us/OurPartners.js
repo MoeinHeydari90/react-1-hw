43ebac29034e924c27d833f3bd0fdd48476a40ba
Moeen Heydari <moeentk@gmail.com>
2024-08-27 11:03:28 +0200

    270824: Move OurPartners into its own component

---
+++ app/about_us/OurPartners.js
@@ -0,0 +1,11 @@
+const OurPartners = () => {
+    return (
+        <>
+            <p>
+                We collaborate with some of the most respected names in the space and technology
+                industries to make every journey extraordinary.
+            </p>
+        </>
+    );
+};
+export default OurPartners;
--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,20 +1,7 @@
 import OurCrew from "./OurCrew";
 import OurValues from "./OurValues";
+import OurPartners from "./OurPartners";
 import styles from "./page.module.css";
-
-// TASK - React 1 week 1
-// After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
-// OurValues.js, OurCrew.js, OurPartners.js should live in this folder
-// import and use the components from the newly created files
-
-const OurPartners = () => {
-    // TASK - React 1 week 1
-    // Create the "Our Crew section"
-    // Use the descriptions provided in /app/about_us/README.md
-    // Use the pictures from /public/crew
-    // Some inspiration ideas found in /data/inspiration_about_us
-    return <p> ADD OUR Partners HERE </p>;
-};
 
 export const Crew = () => {
     return (
@@ -29,9 +16,10 @@ export const Crew = () => {
                     <h2>The crew</h2>
                     <OurCrew />
                 </section>
-
-                {/* TASK - React 1 week 1 */}
-                {/* Add in the "OurPartners" component here */}
+                <section className="card">
+                    <h2>Our Partners</h2>
+                    <OurPartners />
+                </section>
             </main>
         </div>
     );
